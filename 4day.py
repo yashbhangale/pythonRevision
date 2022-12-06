a694df97130a92be7f89a16f4d9d436514537069
yashbhangale <yashbhangale9@gmail.com>
2022-12-06 17:19:01 +0530

    5day added

---
+++ 4day.py
@@ -0,0 +1,20 @@
+#operators
+a = 15+7  # + is an operator
+b = 15*7  # * is an operator 
+c = 15-7  # - is an operator 
+d = 15/7  # / is an operator 
+e = 15//7 # // is an operator 
+f = 15%7  # % is an operator 
+g = 15**7 # ** is an operator 
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+
+
+
---
+++ simple calculator.py
@@ -0,0 +1,14 @@
+# make a calculator using python for addition, subtraction, multiplication, division, and modulo
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+print(f"Addition: {a+b}")
+if input : 10
+print(f"Subtraction: {a-b}")
+print(f"multiplication: {a*b}")
+print(f"division: {a/b}")
+print(f"modulo: {a%b}")
+
+
+
+
