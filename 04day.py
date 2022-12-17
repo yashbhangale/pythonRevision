a303f53a48f7d7513a63b04f112b05d16edeb824
yashbhangale <yashbhangale9@gmail.com>
2022-12-17 14:23:03 +0530

    03 day done

---
+++ 04day.py
@@ -0,0 +1,13 @@
+# comments in python
+#Comments in Python are identified with a hash symbol, #, and extend to the end of the line
+
+
+#for eg
+
+a = 1 
+b = 3 
+c = a + b
+print (c)
+
+
+#also multiply these two numbers
